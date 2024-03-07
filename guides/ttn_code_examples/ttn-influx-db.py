e937d4fc4b7fd3541d85d5ff277dd15acf695cd7
Bjørnar Haugstad Jåtten <bjornjaat@hotmail.com>
2024-03-07 09:33:06 +0100

    change script to write payload directly

--- guides/ttn_code_examples/ttn-influx-db.py
+++ guides/ttn_code_examples/ttn-influx-db.py
@@ -6,7 +6,6 @@ from influxdb_client import Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 import json
 
-
 TTN_ADDRESS = "eu1.cloud.thethings.network"
 TTN_PORT = 1883
 TTN_USER = "<redacted>"
@@ -16,7 +15,7 @@ ttn_client = Client(mqtt.CallbackAPIVersion.VERSION2)
 INFLUX_TOKEN = "<redacted>"
 INFLUX_ORG = "iot2024"
 INFLUX_URL = "http://influx.itu.dk:8086"
-INFLUX_BUCKET = "iot2024"
+INFLUX_BUCKET = "iot2024_final"
 
 measurement_name = "bjornars_measurement"
 
@@ -39,17 +38,23 @@ def on_ttn_connect(client, _userdata, _flags, return_code, _properties):
 # The callback for when a PUBLISH message is received from the server.
 def on_ttn_message(client, userdata, msg):
     message = json.loads(msg.payload)
-    print("message received")
+
+    decoded_co2 = message["uplink_message"]["decoded_payload"].get("co2")
+    decoded_humidity = message["uplink_message"]["decoded_payload"].get("humidity")
+    decoded_temperature = message["uplink_message"]["decoded_payload"].get("temperature")
+    print(decoded_co2, decoded_humidity, decoded_temperature)
     # we recieve and read the relevant information into tags and fields
     # please replace with your name!
     point = Point(measurement_name) \
-            .tag("device_id", message["end_device_ids"]["device_id"]) \
-            .tag("dev_eui", message["end_device_ids"]["dev_eui"]) \
-            .tag("app_eui", message["end_device_ids"]["join_eui"]) \
-            .tag("port", int(message["uplink_message"]["f_port"])) \
-            .field("payload", message["uplink_message"]["frm_payload"]) \
-            .time(message["received_at"])  # Fix the timestamp to be the TTN received time
-    
+        .tag("device_id", message["end_device_ids"]["device_id"]) \
+        .tag("dev_eui", message["end_device_ids"]["dev_eui"]) \
+        .tag("app_eui", message["end_device_ids"]["join_eui"]) \
+        .tag("port", int(message["uplink_message"]["f_port"])) \
+        .field("co2", decoded_co2) \
+        .field("humidity", decoded_humidity) \
+        .field("temperature", decoded_temperature) \
+        .time(message["received_at"])  # Fix the timestamp to be the TTN received time
+        
     print(point)
     write_api.write(bucket=INFLUX_BUCKET, org=INFLUX_ORG, record=point)
 
